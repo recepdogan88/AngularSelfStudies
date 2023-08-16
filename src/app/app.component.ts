c82ab9780147abdc08b2399b237a7e5fa53ca1a7
Dogan Recep <Recep.Dogan@swisscom.com>
2023-08-16 10:07:55 +0200

    to-do component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  name="RECEP DOGAN "
+  user = 'Recep';
+
+  items = [
+    {description: "kahvalti",action:"no"},
+    {description: "spor",action:"no"},
+    {description: "kitap",action:"no"}
+  ]
 }
